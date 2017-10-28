1d06d0f35d9bd7ce6e0017883eea75efdb77ee53
denghaoliang <1377196304@qq.com>
2017-10-28 08:31:57 +0800

    aaa

--- ajax.js
+++ ajax.js
@@ -1,8 +1,22 @@
-$.ajax({
+
+$(function(){
+	
+	var anniu=$("#button")
+	anniu.click(function(){
+		$.ajax({
 		url:"http://apis.juhe.cn/geo/",
 		dataType:'jsonp',
 		processData: false,
 		type:'GET',
 		data:"lng="+jd+"&lat="+wd+"&type=2&dtype=&key=79a40402cc9a01ed1985bae6e8d47358",
-		success:function(data){console.log(document.write(data.result.address)	}
+		success:function(data){document.write(data.result.address)}
 })
+	})
+	
+	
+	
+	
+	
+	
+})
+
